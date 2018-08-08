edb85ffadefd909ee6f11ad6d4eef2784482cfe5
zzq <429393093@qq.com>
2018-08-08 21:47:59 +0800

    第一次提交

---
+++ com.itheima.sadassadsa/src/com/itheima/sadassadsa/User.java
@@ -0,0 +1,15 @@
+package com.itheima.sadassadsa;
+
+public class User {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
